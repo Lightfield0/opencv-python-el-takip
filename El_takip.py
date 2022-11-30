6fa73c560670ba66778f5bdb4cb080e7cec24f07
Lightfield0 <114153309+Lightfield0@users.noreply.github.com>
2022-11-30 14:48:47 +0300

    Add files via upload

---
+++ El_takip.py
@@ -0,0 +1,50 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+
+cap = cv.VideoCapture(0)
+
+mpHands= mp.solutions.hands
+hands= mpHands.Hands()
+mpDraw= mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    ret, img = cap.read()
+    imgRgb= cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    
+    results = hands.process(imgRgb)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+           
+            for id, lm  in enumerate(handLms.landmark):
+                # print(id,lm)
+                h,w,c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                if id ==11:
+                    cv.circle(img,(cx, cy),15,(255,0,0), cv.FILLED)
+                elif id ==10:
+                    cv.circle(img,(cx, cy),15,(255,0,0), cv.FILLED)
+                elif id ==12:
+                    cv.circle(img,(cx, cy),15,(255,0,0), cv.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    
+    
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img,str(int(fps)), (10,70), (cv.FONT_HERSHEY_PLAIN), 3 , (255,0,255),3)
+    cv.imshow('el takip', img)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()          
+cv.destroyAllWindows() 
